10e89047850c2f8f65af5844c92a0eeaa8a68674
Metezori <metezori27@gmail.com>
2026-04-14 14:32:05 -0400

    Update Collatz.py

--- Collatz.py
+++ Collatz.py
@@ -28,27 +28,12 @@ def fast_collatz(n):
 
 #print ("Steps: " + str(fast_collatz(int(input("Number: ")))))
 
-def bitwise_collatz(n): 
-    if not isinstance(n, int) or n < 1:
-        return 0
-    steps = 0
-    while n > 1: 
-        if not (n & 1):
-            tz = (n & -n).bit_length() - 1 #tz is the number of times you can divide n by 2
-            n >>= tz
-            steps += tz
-        else:
-            n = 3 * n + 1
-            steps += 1
-    return steps
-
-#print ("Steps: " + str(bitwise_collatz(int(input("Number: ")))))
 
 def batch_collatz (r):
     most_steps = 0
     ms_number = []
     for n in tqdm.tqdm(range (1,r+1)):
-        steps = bitwise_collatz(n)
+        steps = fast_collatz(n)
         if steps > most_steps:
             most_steps = steps
             ms_number = [n]
